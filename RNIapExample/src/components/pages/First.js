761951c577ad009dc45c3ea027451d42497c9208
JJMoon <jm99tt@gmail.com>
2017-11-11 11:34:17 +0900

    iOS test product for Auto Subscription product id included.

--- RNIapExample/src/components/pages/First.js
+++ RNIapExample/src/components/pages/First.js
@@ -17,8 +17,8 @@ import EStyleSheet from 'react-native-extended-stylesheet';
 
 const itemSkus = {
   ios: [
-    'com.cooni.point1000',
-    'com.cooni.point5000',
+    'auto.subscript.mtt.iap', 'react.iap.consum.1000', 'react.iap.consum.500', // JWMoon
+    // 'com.cooni.point1000', 'com.cooni.point5000', // dooboolab
   ],
   android: [
     'point_1000',
