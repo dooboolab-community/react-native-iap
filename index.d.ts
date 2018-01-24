4ae3b307407eac93ffb4d1c1c16a0fc442ef7dff
hyochan <dooboolab@gmail.com>
2018-01-25 01:17:37 +0900

    RNIap @types support.

---
+++ index.d.ts
@@ -0,0 +1,15 @@
+export interface Skus {
+  ios: string[];
+  android: string[];
+}
+
+export function getItems(skus: Skus) : Promise<any>;
+export function getSubscribeItems(skus: Skus) : Promise<any>;
+export function buyItem(item: string) : Promise<any>;
+export function buySubscribeItem(item: string) : Promise<any>;
+export function refreshAllItems() : Promise<any>;
+
+export function prepareAndroid() : Promise<string> | void;
+export function refreshPurchaseItemsAndroid(type: string | null) : void;
+export function getPurchasedItemsAndroid(type: string | null) : Promise<any> | void;
+export function consumeItemAndroid(token: string) : Promise<string> | void;
