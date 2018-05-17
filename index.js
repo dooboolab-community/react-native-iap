2cdaad9391a6a02fc287d3fcb2836d03ee9fe14f
Jongwoo Moon <jm99tt@gmail.com>
2018-05-17 16:27:03 +0900

    Remove filtering in getSubscription on iOS

--- index.js
+++ index.js
@@ -53,7 +53,7 @@ export const getProducts = (skus) => Platform.select({
  */
 export const getSubscriptions = (skus) => Platform.select({
   ios: () => RNIapIos.getItems(skus)
-    .then(items => items.filter(item => (!item.type || item.type === IOS_ITEM_TYPE_SUBSCRIPTION) && skus.indexOf(item.productId) > -1)),
+    .then(items => items.filter(item => item.productId)),
   android: () => RNIapModule.getItemsByType(ANDROID_ITEM_TYPE_SUBSCRIPTION, skus)
 })();
 
