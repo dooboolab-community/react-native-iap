418697c244d1d4bff57e7e77d9cbd03b978419d1
Daniel Schlaug <daniel@schlaug.com>
2019-07-09 12:21:47 +0900

    Fix example app not dealing with symlink deps bug

--- IapExample/metro.config.js
+++ IapExample/metro.config.js
@@ -15,3 +15,18 @@ module.exports = {
     }),
   },
 };
+
+const fs = require('fs');
+const getDevPaths = require('get-dev-paths');
+
+const projectRoot = __dirname;
+module.exports = {
+  // Old way
+  getProjectRoots: () => Array.from(new Set(
+    getDevPaths(projectRoot).map($ => fs.realpathSync($))
+  )),
+  // New way
+  watchFolders: Array.from(new Set(
+    getDevPaths(projectRoot).map($ => fs.realpathSync($))
+  ))
+}
