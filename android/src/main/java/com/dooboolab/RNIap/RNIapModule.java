c27e65d8f1d8c274c17fdce5af380aba8fe9b283
hyochan <dooboolab@gmail.com>
2019-10-07 22:10:13 +0900

    Resolve #765

--- android/src/main/java/com/dooboolab/RNIap/RNIapModule.java
+++ android/src/main/java/com/dooboolab/RNIap/RNIapModule.java
@@ -503,7 +503,7 @@ public class RNIapModule extends ReactContextBaseJavaModule implements Purchases
       error.putString("code", errorData[0]);
       error.putString("message", errorData[1]);
       sendEvent(reactContext, "purchase-error", error);
-      DoobooUtils.getInstance().rejectPromiseWithBillingError(promise, billingResult.getResponseCode());
+      DoobooUtils.getInstance().rejectPromisesWithBillingError(PROMISE_BUY_ITEM, billingResult.getResponseCode());
       return;
     }
 
