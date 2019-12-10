1eb5a226d541f4fe545ae1ff9ba1a7d9bd34b433
hyochan <dooboolab@gmail.com>
2019-12-10 14:33:36 +0900

    Upgrade example to react-native@0.61

--- IapExample/metro.config.js
+++ IapExample/metro.config.js
@@ -5,22 +5,7 @@
  * @format
  */
 
-const fs = require('fs');
-const blacklist = require('metro-config/src/defaults/blacklist');
-const path = require('path');
-
 module.exports = {
-  // Enables metro to find the symlinked react-native-iap dependency though it does not support symlinks.
-  // Inspired by https://blog.callstack.io/adding-an-example-app-to-your-react-native-library-d23b9741a19c
-  watchFolders: [path.resolve(__dirname, '..')],
-  resolver: {
-    providesModuleNodeModules: ['react-native', 'react', '@babel/runtime'],
-    blacklistRE: blacklist([
-      new RegExp(
-        `^${escape(path.resolve(__dirname, '..', 'node_modules'))}\\/.*$`,
-      ),
-    ]),
-  },
   transformer: {
     getTransformOptions: async () => ({
       transform: {
