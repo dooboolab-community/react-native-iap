891830fc52ab2e5a5cd576b92846b47513a25050
Andres Aguilar <andresesfm@gmail.com>
2021-05-26 19:38:54 -0700

    Fire tv detection (#1356)
    * Added More ways to detect Amazon Devices
    
    * fixed imports, updated yarn.lock
    
    * fixed missing bracket
    
    Co-authored-by: andres.aguilar <andres.aguilar@nfl.com>

--- android/src/main/java/com/dooboolab/RNIap/DoobooUtils.java
+++ android/src/main/java/com/dooboolab/RNIap/DoobooUtils.java
@@ -1,5 +1,6 @@
 package com.dooboolab.RNIap;
 
+import android.os.Build;
 import android.util.Log;
 
 import com.android.billingclient.api.BillingClient;
@@ -270,8 +271,17 @@ public class DoobooUtils {
     return array;
   }
 
+    private static final String AMAZON_FEATURE_FIRE_TV = "amazon.hardware.fire_tv";
+  private static final String AMAZON_FIRE_TV_MODEL_PREFIX = "AFT";
 
   /**
+   * Detects Stores:
+   * Amazon tablets and Fire TV are considered as APPSTORE_AMAZON
+   * using recommended detection logic from:
+   * https://developer.amazon.com/docs/fire-tv/identify-amazon-fire-tv-devices.html
+   * @param context Application context
+   * @return One of APPSTORE_AMAZON,APPSTORE_GOOGLE,APPSTORE_UNKNOWN
+   *
    * Suppressing deprecation since the alternative requires API level 30
    */
   @SuppressWarnings("deprecation")
@@ -280,15 +290,16 @@ public class DoobooUtils {
     PackageManager pkgManager = appContext.getPackageManager();
     String installerPackageName = pkgManager.getInstallerPackageName(appContext.getPackageName());
 
-    if (installerPackageName == null) {
-      return APPSTORE_UNKNOWN;
+    if (pkgManager.hasSystemFeature(AMAZON_FEATURE_FIRE_TV) ||
+            (installerPackageName!=null && installerPackageName.startsWith("com.amazon.")) ||
+            Build.MODEL.startsWith(AMAZON_FIRE_TV_MODEL_PREFIX)) {
+      Log.d(TAG, "Yes, this is a Fire TV device.");
+      return APPSTORE_AMAZON;
     } else if ("com.android.vending".equals(installerPackageName)) {
       return APPSTORE_GOOGLE;
-    } else if (installerPackageName.startsWith("com.amazon.")) {
-      return APPSTORE_AMAZON;
-    } else {
+    }  else {
       Log.d(TAG, "Unknown installer source: " + installerPackageName);
+      return APPSTORE_UNKNOWN;
     }
-    return APPSTORE_UNKNOWN;
   }
 }
