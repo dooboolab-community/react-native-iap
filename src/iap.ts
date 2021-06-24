ab2c4923e77d34d16d45ed48e89df1057c157aa1
andres.aguilar <andres.aguilar@nfl.com>
2021-06-24 15:34:02 -0700

    set expected type instead of string

--- src/iap.ts
+++ src/iap.ts
@@ -566,9 +566,9 @@ export const consumePurchaseAndroid = (
 /**
  * Should Add Store Payment (iOS only)
  *   Indicates the the App Store purchase should continue from the app instead of the App Store.
- * @returns {Promise<string>} productId of the promoted product
+ * @returns {Promise<Product | null>} promoted product
  */
-export const getPromotedProductIOS = (): Promise<string> =>
+export const getPromotedProductIOS = (): Promise<Product | null> =>
   (
     Platform.select({
       ios: async () => {
