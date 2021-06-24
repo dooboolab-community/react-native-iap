f8cdee452b4bb6899d7953d57de2e121055254fb
Andres Aguilar <andresesfm@gmail.com>
2021-06-24 02:17:12 -0700

    implement requested client-side validation for amazon purchases (#1392)
    * implement requested client-side validation for amazon purchases
    
    * fix lint
    
    Co-authored-by: andres.aguilar <andres.aguilar@nfl.com>

---
+++ src/types/amazon.ts
@@ -0,0 +1,23 @@
+/**
+ * From: https://developer.amazon.com/es/docs/in-app-purchasing/iap-rvs-examples.html
+ */
+export type ReceiptType = {
+  autoRenewing: boolean;
+  betaProduct: boolean;
+  cancelDate: number | null;
+  cancelReason: string;
+  deferredDate: number | null;
+  deferredSku: number | null;
+  freeTrialEndDate: number;
+  gracePeriodEndDate: number;
+  parentProductId: string;
+  productId: string;
+  productType: string;
+  purchaseDate: number;
+  quantity: number;
+  receiptId: string;
+  renewalDate: number;
+  term: string;
+  termSku: string;
+  testTransaction: boolean;
+} & Record<string, unknown>;
