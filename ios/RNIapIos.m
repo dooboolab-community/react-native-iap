a6ad9d6e7940105e063b4d4c074a9d15aad60882
andres.aguilar <andres.aguilar@nfl.com>
2021-08-16 10:59:27 -0700

    fixed naming errors from objc

--- ios/RNIapIos.m
+++ ios/RNIapIos.m
@@ -3,35 +3,46 @@
 #import <React/RCTBridgeModule.h>
 
 @interface RCT_EXTERN_MODULE (RNIapIos, NSObject)
-RCT_EXTERN_METHOD(initConnection:(RCTPromiseResolveBlock)resolve 
-                  reject:(RCTPromiseRejectBlock)reject) 
-RCT_EXTERN_METHOD(endConnection:(RCTPromiseResolveBlock)resolve 
+RCT_EXTERN_METHOD(initConnection:
+                  (RCTPromiseResolveBlock)resolve
                   reject:(RCTPromiseRejectBlock)reject)
-RCT_EXTERN_METHOD(getItems:(NSArray*)skus 
+RCT_EXTERN_METHOD(endConnection:
+                  (RCTPromiseResolveBlock)resolve
+                  reject:(RCTPromiseRejectBlock)reject)
+RCT_EXTERN_METHOD(getItems:
+                  (NSArray*)skus
                   resolve:(RCTPromiseResolveBlock)resolve 
                   reject:(RCTPromiseRejectBlock)reject)
-RCT_EXTERN_METHOD(getAvailableItems:(RCTPromiseResolveBlock)resolve 
+RCT_EXTERN_METHOD(getAvailableItems:
+                  (RCTPromiseResolveBlock)resolve
                   reject:(RCTPromiseRejectBlock)reject)
-RCT_EXTERN_METHOD(buyProduct:(NSString*)sku andDangerouslyFinishTransactionAutomatically:(BOOL)finishAutomatically
+RCT_EXTERN_METHOD(buyProduct:
+                  (NSString*)sku
+                  andDangerouslyFinishTransactionAutomatically:(BOOL)finishAutomatically
                   resolve:(RCTPromiseResolveBlock)resolve
                   reject:(RCTPromiseRejectBlock)reject)
-RCT_EXTERN_METHOD(buyProductWithOffer:(NSString*)sku
+RCT_EXTERN_METHOD(buyProductWithOffer:
+                  (NSString*)sku
                   forUser:(NSString*)usernameHash
                   withOffer:(NSDictionary*)discountOffer
                   resolve:(RCTPromiseResolveBlock)resolve
                   reject:(RCTPromiseRejectBlock)reject)
-RCT_EXTERN_METHOD(buyProductWithQuantityIOS:(NSString*)sku
+RCT_EXTERN_METHOD(buyProductWithQuantityIOS:
+                  (NSString*)sku
                   quantity:(NSInteger)quantity
                   resolve:(RCTPromiseResolveBlock)resolve
                   reject:(RCTPromiseRejectBlock)reject)
-RCT_EXTERN_METHOD(clearTransaction:(RCTPromiseResolveBlock)resolve
+RCT_EXTERN_METHOD(clearTransaction:
+                  (RCTPromiseResolveBlock)resolve
                   reject:(RCTPromiseRejectBlock)reject)
 RCT_EXTERN_METHOD(clearProducts) 
-RCT_EXTERN_METHOD(promotedProduct:(RCTPromiseResolveBlock)resolve
+RCT_EXTERN_METHOD(promotedProduct:
+                  (RCTPromiseResolveBlock)resolve
                   reject:(RCTPromiseRejectBlock)reject)
 RCT_EXTERN_METHOD(buyPromotedProduct:(RCTPromiseResolveBlock)resolve
                   reject:(RCTPromiseRejectBlock)reject)
-RCT_EXTERN_METHOD(requestReceipt:(BOOL)refresh
+RCT_EXTERN_METHOD(requestReceipt:
+                  (BOOL)refresh
                   resolve:(RCTPromiseResolveBlock)resolve
                   reject:(RCTPromiseRejectBlock)reject) {
     [self requestReceiptDataWithBlock:refresh withBlock:^(NSData *receiptData, NSError *error) {
