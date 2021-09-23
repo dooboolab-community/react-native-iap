598dd5a7c78ecc3bacb34262b752974ac2f1e333
andres.aguilar <andres.aguilar@nfl.com>
2021-09-23 14:27:26 -0700

    removed deprecated method

--- android/src/play/java/com/dooboolab/RNIap/RNIapPackage.kt
+++ android/src/play/java/com/dooboolab/RNIap/RNIapPackage.kt
@@ -45,9 +45,6 @@ import com.facebook.react.bridge.NativeModule
 import com.facebook.react.uimanager.ViewManager
 
 class RNIapPackage : ReactPackage {
-    override fun createJSModules(): List<Class<out JavaScriptModule?>> {
-        return emptyList()
-    }
 
     override fun createViewManagers(reactContext: ReactApplicationContext): List<ViewManager<*, *>> {
         return emptyList()
