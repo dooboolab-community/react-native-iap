c22265d923cd6ca03791b76db189c7fccb378d6e
zxcpoiu <zxcpoiu@users.noreply.github.com>
2021-11-15 17:32:56 +0800

    android: remove deprecated method createJSModules in module package (#1568)
    RN 0.47 deprecated the method.
    see: https://github.com/facebook/react-native/commit/ce6fb337a146e6f261f2afb564aa19363774a7a8

--- android/src/amazon/java/com/dooboolab/RNIap/RNIapPackage.kt
+++ android/src/amazon/java/com/dooboolab/RNIap/RNIapPackage.kt
@@ -38,10 +38,6 @@ import com.facebook.react.uimanager.ViewManager
 
 class RNIapPackage : ReactPackage {
 
-    override fun createJSModules(): MutableList<Class<out JavaScriptModule>> {
-        return mutableListOf()
-    }
-
     override fun createViewManagers(reactContext: ReactApplicationContext): List<ViewManager<*, *>> {
         return emptyList()
     }
