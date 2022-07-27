aa5ec19be2ad15441e0915809d16a98f38287ee9
Andres Aguilar <andresesfm@gmail.com>
2022-07-26 20:36:38 -0700

    Uses local ios files in Example app (#1759)
    * Uses local ios files in Example app
    
    * Add react-native.config
    
    Co-authored-by: Andres Aguilar <andres.aguilar@nfl.com>

---
+++ IapExample/react-native.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    dependencies: {
+      'react-native-iap': {
+        platforms: {
+          ios: null, // disable ios platform, so we can use local filess
+        },
+      },
+    },
+  };
