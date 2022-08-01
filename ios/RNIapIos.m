fafdffd1ae409d452ee8cb0178aa0469af97929d
Charles Parker <charlesparker@FiltaMacbook.local>
2022-07-31 21:08:12 -0700

    Fix mismatch between buyProduct signatues in Obj-C and swift

--- ios/RNIapIos.m
+++ ios/RNIapIos.m
@@ -18,7 +18,6 @@ RCT_EXTERN_METHOD(getAvailableItems:
                   reject:(RCTPromiseRejectBlock)reject)
 RCT_EXTERN_METHOD(buyProduct:
                   (NSString*)sku
-                  appAccountToken:(NSString*)appAccountToken
                   andDangerouslyFinishTransactionAutomatically:(BOOL)andDangerouslyFinishTransactionAutomatically
                   applicationUsername:(NSString)applicationUsername
                   resolve:(RCTPromiseResolveBlock)resolve
