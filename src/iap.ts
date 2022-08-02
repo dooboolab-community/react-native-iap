3ac3871b399bab440ce4d172723673aa0b516631
Jérémy Barbet <jeremgraph@gmail.com>
2022-08-02 20:13:22 +0000

    fix: methods types (#1792)

--- src/iap.ts
+++ src/iap.ts
@@ -259,9 +259,9 @@ export const getAvailablePurchases = (): Promise<
 export const requestPurchase = ({
   sku,
   andDangerouslyFinishTransactionAutomaticallyIOS = false,
-  obfuscatedAccountIdAndroid = undefined,
-  obfuscatedProfileIdAndroid = undefined,
-  applicationUsername = undefined,
+  obfuscatedAccountIdAndroid,
+  obfuscatedProfileIdAndroid,
+  applicationUsername,
 }: RequestPurchase): Promise<InAppPurchase> =>
   (
     Platform.select({
@@ -305,11 +305,11 @@ export const requestPurchase = ({
 export const requestSubscription = ({
   sku,
   andDangerouslyFinishTransactionAutomaticallyIOS = false,
-  purchaseTokenAndroid = undefined,
+  purchaseTokenAndroid,
   prorationModeAndroid = -1,
-  obfuscatedAccountIdAndroid = undefined,
-  obfuscatedProfileIdAndroid = undefined,
-  applicationUsername = undefined,
+  obfuscatedAccountIdAndroid,
+  obfuscatedProfileIdAndroid,
+  applicationUsername,
 }: RequestSubscription): Promise<SubscriptionPurchase | null> =>
   (
     Platform.select({
