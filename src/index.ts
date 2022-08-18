2277b80c669f1777f3aa5a8f32b687d474d330f9
Jérémy Barbet <jeremgraph@gmail.com>
2022-08-18 07:58:44 +0000

    refactor: extract event-emitter to specific file

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 export * from './iap';
 export * from './types';
+export * from './eventEmitter';
 export * from './hooks/useIAP';
 export * from './hooks/withIAPContext';
 export * from './purchaseError';
