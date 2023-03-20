4a2db36218dd7d5eb7f9200059f7a2fff93bb7a6
Andres Aguilar <andres.aguilar@nfl.com>
2023-03-20 10:33:17 -0700

    updated ktlint and formatted

--- android/src/main/java/com/dooboolab/RNIap/PromiseUtils.kt
+++ android/src/main/java/com/dooboolab/RNIap/PromiseUtils.kt
@@ -28,7 +28,7 @@ object PromiseUtils {
         key: String,
         code: String?,
         message: String?,
-        err: Exception?
+        err: Exception?,
     ) {
         promises[key]?.forEach { promise ->
             promise.safeReject(code, message, err)
--- android/src/play/java/com/dooboolab/RNIap/PlayUtils.kt
+++ android/src/play/java/com/dooboolab/RNIap/PlayUtils.kt
@@ -17,67 +17,67 @@ object PlayUtils {
             BillingClient.BillingResponseCode.FEATURE_NOT_SUPPORTED -> {
                 BillingResponse(
                     PromiseUtils.E_SERVICE_ERROR,
-                    "This feature is not available on your device."
+                    "This feature is not available on your device.",
                 )
             }
             BillingClient.BillingResponseCode.SERVICE_DISCONNECTED -> {
                 BillingResponse(
                     PromiseUtils.E_NETWORK_ERROR,
-                    "The service is disconnected (check your internet connection.)"
+                    "The service is disconnected (check your internet connection.)",
                 )
             }
             BillingClient.BillingResponseCode.OK -> {
                 BillingResponse(
                     "OK",
-                    ""
+                    "",
                 )
             }
             BillingClient.BillingResponseCode.USER_CANCELED -> {
                 BillingResponse(
                     PromiseUtils.E_USER_CANCELLED,
-                    "Payment is Cancelled."
+                    "Payment is Cancelled.",
                 )
             }
             BillingClient.BillingResponseCode.SERVICE_UNAVAILABLE -> {
                 BillingResponse(
                     PromiseUtils.E_SERVICE_ERROR,
-                    "The service is unreachable. This may be your internet connection, or the Play Store may be down."
+                    "The service is unreachable. This may be your internet connection, or the Play Store may be down.",
                 )
             }
             BillingClient.BillingResponseCode.BILLING_UNAVAILABLE -> {
                 BillingResponse(
                     PromiseUtils.E_SERVICE_ERROR,
-                    "Billing is unavailable. This may be a problem with your device, or the Play Store may be down."
+                    "Billing is unavailable. This may be a problem with your device, or the Play Store may be down.",
                 )
             }
             BillingClient.BillingResponseCode.ITEM_UNAVAILABLE -> {
                 BillingResponse(
                     PromiseUtils.E_ITEM_UNAVAILABLE,
-                    "That item is unavailable."
+                    "That item is unavailable.",
                 )
             }
             BillingClient.BillingResponseCode.DEVELOPER_ERROR -> {
                 BillingResponse(
                     PromiseUtils.E_DEVELOPER_ERROR,
-                    "Google is indicating that we have some issue connecting to payment."
+                    "Google is indicating that we have some issue connecting to payment.",
                 )
             }
             BillingClient.BillingResponseCode.ERROR -> {
                 BillingResponse(
                     PromiseUtils.E_UNKNOWN,
-                    "An unknown or unexpected error has occurred. Please try again later."
+                    "An unknown or unexpected error has occurred. Please try again later.",
                 )
             }
             BillingClient.BillingResponseCode.ITEM_ALREADY_OWNED -> {
                 BillingResponse(
                     PromiseUtils.E_ALREADY_OWNED,
-                    "You already own this item."
+                    "You already own this item.",
                 )
             }
             else -> {
                 BillingResponse(
                     PromiseUtils.E_UNKNOWN,
-                    "Purchase failed with code: $responseCode"
+                    "Purchase failed with code: $responseCode",
                 )
             }
         }
