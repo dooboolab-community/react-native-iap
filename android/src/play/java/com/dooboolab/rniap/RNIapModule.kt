2f0403a0a2248378de97853e33585d065e95de7b
Mkolakoglu <mustafakolakoglu@gmail.com>
2023-04-06 23:30:32 +0200

    ensureConnection - fixed rejection handling (#2316)
    Hi, I was trying to create a scenario that we can not establish
    BillingClient connection. To do that, I simply changed `initConnection`
    as below:
    
    ```
    fun initConnection(promise: Promise) {
             Log.i(TAG, "Google Play Services are not available on this device")
             promise.safeReject(PromiseUtils.E_NOT_PREPARED, "Google Play Services are not available on this device")
             return
    }
    ```
       
    After observing that scenario, I noticed that `ensureConnection` is not
    parsing the rejection parameters properly and it's only logging this
    info in the logcat, which is quite dangerous because it's happening
    silently and probably none of us is aware of this issue so far.
    
    When it's rejected, there is only 1 item in `it` array and here is the
    output of `it[0]`:
    ![Screen Shot 2023-03-16 at 13 03
    08](https://user-images.githubusercontent.com/16003411/225621284-bec5c01c-7ae1-4cd0-bc6a-8a9d2c8d0b00.png)
    
    I tried to fix the parameter handling and rejected the case when the
    parameters are incorrect instead of just logging it. So that, we will be
    aware of if anything goes wrong.
    
    ---------
    
    Co-authored-by: Andres Aguilar <andresesfm@gmail.com>

--- android/src/play/java/com/dooboolab/rniap/RNIapModule.kt
+++ android/src/play/java/com/dooboolab/rniap/RNIapModule.kt
@@ -69,16 +69,29 @@ class RNIapModule(
                             promise.safeReject(PromiseUtils.E_NOT_PREPARED, "Unable to auto-initialize connection")
                         }
                     } else {
+                        promise.safeReject(PromiseUtils.E_UNKNOWN, "ensureConnection - incorrect parameter in resolve")
                         Log.i(TAG, "Incorrect parameter in resolve")
                     }
                 },
                 {
+                    var errorCode: String? = null
+                    var errorMessage: String? = null
                     if (it.size > 1 && it[0] is String && it[1] is String) {
+                        errorCode = it[0] as String
+                        errorMessage = it[1] as String
+                    } else if (it.isNotEmpty() && it[0] is WritableNativeMap) {
+                        val errorMap = it[0] as WritableNativeMap
+                        errorCode = errorMap.getString("code")
+                        errorMessage = errorMap.getString("message")
+                    }
+
+                    if (errorCode is String && errorMessage is String) {
                         promise.safeReject(
-                            it[0] as String,
-                            it[1] as String,
+                            errorCode,
+                            errorMessage,
                         )
                     } else {
+                        promise.safeReject(PromiseUtils.E_UNKNOWN, "ensureConnection - incorrect parameter in reject")
                         Log.i(TAG, "Incorrect parameters in reject")
                     }
                 },
