052e5c56e766563f2efd0777c8beb0537735b26a
Nippert <quentin.n@apps-panel.com>
2023-08-02 14:27:22 +0200

    fix: added missing iOS appAccountToken on ProductPurchase TS interface

--- src/types/index.ts
+++ src/types/index.ts
@@ -69,6 +69,7 @@ export interface ProductPurchase {
   originalTransactionDateIOS?: number;
   originalTransactionIdentifierIOS?: string;
   verificationResultIOS?: string;
+  appAccountToken?: string;
   //Android
   productIds?: string[];
   dataAndroid?: string;
