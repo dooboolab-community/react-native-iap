4da355728cdb23e6994e1cbab8abff8ed144d524
Arthur Geron <arthurgeron@hotmail.com>
2023-11-08 13:43:36 -0300

    feat: create LatestPromiseKeeper

---
+++ ios/LatestPromiseKeeper.swift
@@ -0,0 +1,26 @@
+import ThreadSafe
+
+// Only keeps latest promise, assumes older promises are not needed
+// Avoids racing conditions by storing latestPromise in a thread safe var
+// Cancels previous promises when new ones are added
+// Should not be used when all promises are relevant (e.g. Purchases)
+class LatestPromiseKeeper {
+    private var latestPromise: ThreadSafe<(RCTPromiseResolveBlock, RCTPromiseRejectBlock)?> = ThreadSafe(nil)
+    private var latestRequest: ThreadSafe<SKProductsRequest?> = ThreadSafe(nil)
+
+    func setLatestPromise(request: SKProductsRequest, resolve: @escaping RCTPromiseResolveBlock, reject: @escaping RCTPromiseRejectBlock) {
+        cancelOngoingRequest()
+
+        latestRequest.atomically { $0 = request }
+        latestPromise.atomically { $0 = (resolve, reject) }
+    }
+
+    func cancelOngoingRequest() {
+        latestRequest.atomically { ongoingRequest in
+            ongoingRequest?.cancel()
+            ongoingRequest = nil
+        }
+
+        latestPromise.atomically { $0 = nil }
+    }
+}
