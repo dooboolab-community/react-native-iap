b1e1513d2564931229e29fe9d450b8af81fa306a
Arthur Geron <arthurgeron@hotmail.com>
2023-11-08 14:29:49 -0300

    fix: allow atomically value to be modified

--- ios/ThreadSafe.swift
+++ ios/ThreadSafe.swift
@@ -1,18 +1,18 @@
 class ThreadSafe<A> {
     private var _value: A
     private let queue = DispatchQueue(label: "ThreadSafe")
-  
+    
     init(_ value: A) {
         self._value = value
     }
-  
+    
     var value: A {
         return queue.sync { _value }
     }
-  
-    func atomically(_ transform: (inout A) -> Void) {
+    
+    func atomically(_ transform: (inout A) -> ()) {
         queue.sync {
-            transform(&_value)
+            transform(&self._value)
         }
     }
 }
