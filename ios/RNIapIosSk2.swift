9b44148dc19147ce981593b56ad4eb768cb2af5e
cervenkovab <cervenkovab@gmail.com>
2023-11-17 13:53:32 +0100

    feat: add renewal info for Storekit 2

--- ios/RNIapIosSk2.swift
+++ ios/RNIapIosSk2.swift
@@ -280,7 +280,7 @@ class RNIapIosSk2: RCTEventEmitter, Sk2Delegate {
     override func addListener(_ eventName: String?) {
         super.addListener(eventName)
     }
-
+    
     /**
      "iap-transaction-updated" is unique to Sk2.
      "iap-promoted-product" is only avaiable on Sk1
