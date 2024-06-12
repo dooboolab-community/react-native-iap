f573b020ec7dd78b7f5a0e8178671dd2dc2341fc
hyochan <dooboolab@gmail.com>
2024-06-12 16:42:04 +0900

    fix: ktlint

--- android/src/amazon/java/com/dooboolab/rniap/PurchasingServiceProxyAmazonImpl.kt
+++ android/src/amazon/java/com/dooboolab/rniap/PurchasingServiceProxyAmazonImpl.kt
@@ -7,27 +7,21 @@ import com.amazon.device.iap.model.FulfillmentResult
 import com.amazon.device.iap.model.RequestId
 
 class PurchasingServiceProxyAmazonImpl : PurchasingServiceProxy {
-    override fun registerListener(var0: Context?, var1: PurchasingListener?) {
-        return PurchasingService.registerListener(var0, var1)
-    }
+    override fun registerListener(
+        var0: Context?,
+        var1: PurchasingListener?,
+    ) = PurchasingService.registerListener(var0, var1)
 
-    override fun getUserData(): RequestId {
-        return PurchasingService.getUserData()
-    }
+    override fun getUserData(): RequestId = PurchasingService.getUserData()
 
-    override fun purchase(var0: String?): RequestId {
-        return PurchasingService.purchase(var0)
-    }
+    override fun purchase(var0: String?): RequestId = PurchasingService.purchase(var0)
 
-    override fun getProductData(var0: Set<String?>?): RequestId {
-        return PurchasingService.getProductData(var0)
-    }
+    override fun getProductData(var0: Set<String?>?): RequestId = PurchasingService.getProductData(var0)
 
-    override fun getPurchaseUpdates(var0: Boolean): RequestId {
-        return PurchasingService.getPurchaseUpdates(var0)
-    }
+    override fun getPurchaseUpdates(var0: Boolean): RequestId = PurchasingService.getPurchaseUpdates(var0)
 
-    override fun notifyFulfillment(var0: String?, var1: FulfillmentResult?) {
-        return PurchasingService.notifyFulfillment(var0, var1)
-    }
+    override fun notifyFulfillment(
+        var0: String?,
+        var1: FulfillmentResult?,
+    ) = PurchasingService.notifyFulfillment(var0, var1)
 }
--- android/src/play/java/com/dooboolab/rniap/PlayUtils.kt
+++ android/src/play/java/com/dooboolab/rniap/PlayUtils.kt
@@ -4,94 +4,104 @@ import android.util.Log
 import com.android.billingclient.api.BillingClient
 import com.facebook.react.bridge.Promise
 
-data class BillingResponse(val code: String, val message: String)
+data class BillingResponse(
+    val code: String,
+    val message: String,
+)
 
 object PlayUtils {
-    fun rejectPromiseWithBillingError(promise: Promise, responseCode: Int) {
+    fun rejectPromiseWithBillingError(
+        promise: Promise,
+        responseCode: Int,
+    ) {
         val errorData = getBillingResponseData(responseCode)
         promise.safeReject(errorData.code, errorData.message)
     }
 
     fun getBillingResponseData(responseCode: Int): BillingResponse {
-        val errorData = when (responseCode) {
-            BillingClient.BillingResponseCode.FEATURE_NOT_SUPPORTED -> {
-                BillingResponse(
-                    PromiseUtils.E_SERVICE_ERROR,
-                    "This feature is not available on your device.",
-                )
+        val errorData =
+            when (responseCode) {
+                BillingClient.BillingResponseCode.FEATURE_NOT_SUPPORTED -> {
+                    BillingResponse(
+                        PromiseUtils.E_SERVICE_ERROR,
+                        "This feature is not available on your device.",
+                    )
+                }
+                BillingClient.BillingResponseCode.SERVICE_DISCONNECTED -> {
+                    BillingResponse(
+                        PromiseUtils.E_NETWORK_ERROR,
+                        "The service is disconnected (check your internet connection.)",
+                    )
+                }
+                BillingClient.BillingResponseCode.NETWORK_ERROR -> {
+                    BillingResponse(
+                        PromiseUtils.E_NETWORK_ERROR,
+                        "You have problem with network connection.",
+                    )
+                }
+                BillingClient.BillingResponseCode.OK -> {
+                    BillingResponse(
+                        "OK",
+                        "",
+                    )
+                }
+                BillingClient.BillingResponseCode.USER_CANCELED -> {
+                    BillingResponse(
+                        PromiseUtils.E_USER_CANCELLED,
+                        "Payment is Cancelled.",
+                    )
+                }
+                BillingClient.BillingResponseCode.SERVICE_UNAVAILABLE -> {
+                    BillingResponse(
+                        PromiseUtils.E_SERVICE_ERROR,
+                        "The service is unreachable. This may be your internet connection, or the Play Store may be down.",
+                    )
+                }
+                BillingClient.BillingResponseCode.BILLING_UNAVAILABLE -> {
+                    BillingResponse(
+                        PromiseUtils.E_SERVICE_ERROR,
+                        "Billing is unavailable. This may be a problem with your device, or the Play Store may be down.",
+                    )
+                }
+                BillingClient.BillingResponseCode.ITEM_UNAVAILABLE -> {
+                    BillingResponse(
+                        PromiseUtils.E_ITEM_UNAVAILABLE,
+                        "That item is unavailable.",
+                    )
+                }
+                BillingClient.BillingResponseCode.DEVELOPER_ERROR -> {
+                    BillingResponse(
+                        PromiseUtils.E_DEVELOPER_ERROR,
+                        "Google is indicating that we have some issue connecting to payment.",
+                    )
+                }
+                BillingClient.BillingResponseCode.ERROR -> {
+                    BillingResponse(
+                        PromiseUtils.E_UNKNOWN,
+                        "An unknown or unexpected error has occurred. Please try again later.",
+                    )
+                }
+                BillingClient.BillingResponseCode.ITEM_ALREADY_OWNED -> {
+                    BillingResponse(
+                        PromiseUtils.E_ALREADY_OWNED,
+                        "You already own this item.",
+                    )
+                }
+                else -> {
+                    BillingResponse(
+                        PromiseUtils.E_UNKNOWN,
+                        "Purchase failed with code: $responseCode",
+                    )
+                }
             }
-            BillingClient.BillingResponseCode.SERVICE_DISCONNECTED -> {
-                BillingResponse(
-                    PromiseUtils.E_NETWORK_ERROR,
-                    "The service is disconnected (check your internet connection.)",
-                )
-            }
-            BillingClient.BillingResponseCode.NETWORK_ERROR -> {
-                BillingResponse(
-                    PromiseUtils.E_NETWORK_ERROR,
-                    "You have problem with network connection.",
-                )
-            }
-            BillingClient.BillingResponseCode.OK -> {
-                BillingResponse(
-                    "OK",
-                    "",
-                )
-            }
-            BillingClient.BillingResponseCode.USER_CANCELED -> {
-                BillingResponse(
-                    PromiseUtils.E_USER_CANCELLED,
-                    "Payment is Cancelled.",
-                )
-            }
-            BillingClient.BillingResponseCode.SERVICE_UNAVAILABLE -> {
-                BillingResponse(
-                    PromiseUtils.E_SERVICE_ERROR,
-                    "The service is unreachable. This may be your internet connection, or the Play Store may be down.",
-                )
-            }
-            BillingClient.BillingResponseCode.BILLING_UNAVAILABLE -> {
-                BillingResponse(
-                    PromiseUtils.E_SERVICE_ERROR,
-                    "Billing is unavailable. This may be a problem with your device, or the Play Store may be down.",
-                )
-            }
-            BillingClient.BillingResponseCode.ITEM_UNAVAILABLE -> {
-                BillingResponse(
-                    PromiseUtils.E_ITEM_UNAVAILABLE,
-                    "That item is unavailable.",
-                )
-            }
-            BillingClient.BillingResponseCode.DEVELOPER_ERROR -> {
-                BillingResponse(
-                    PromiseUtils.E_DEVELOPER_ERROR,
-                    "Google is indicating that we have some issue connecting to payment.",
-                )
-            }
-            BillingClient.BillingResponseCode.ERROR -> {
-                BillingResponse(
-                    PromiseUtils.E_UNKNOWN,
-                    "An unknown or unexpected error has occurred. Please try again later.",
-                )
-            }
-            BillingClient.BillingResponseCode.ITEM_ALREADY_OWNED -> {
-                BillingResponse(
-                    PromiseUtils.E_ALREADY_OWNED,
-                    "You already own this item.",
-                )
-            }
-            else -> {
-                BillingResponse(
-                    PromiseUtils.E_UNKNOWN,
-                    "Purchase failed with code: $responseCode",
-                )
-            }
-        }
         Log.e(TAG, "Error Code : $responseCode")
         return errorData
     }
 
-    fun rejectPromisesWithBillingError(key: String, responseCode: Int) {
+    fun rejectPromisesWithBillingError(
+        key: String,
+        responseCode: Int,
+    ) {
         val errorData = getBillingResponseData(responseCode)
         PromiseUtils.rejectPromisesForKey(key, errorData.code, errorData.message, null)
     }
