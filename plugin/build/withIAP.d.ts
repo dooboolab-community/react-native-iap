5a212665245ae03509e0529bf1200a31f9a76bf3
hyochan <dooboolab@gmail.com>
2024-06-21 14:59:22 +0900

    chore(ios): remove flipper

--- plugin/build/withIAP.d.ts
+++ plugin/build/withIAP.d.ts
@@ -1,12 +1,9 @@
-import {ConfigPlugin} from 'expo/config-plugins';
+import { ConfigPlugin } from 'expo/config-plugins';
 type PaymentProvider = 'Amazon AppStore' | 'both' | 'Play Store';
-export declare const modifyAppBuildGradle: (
-  buildGradle: string,
-  paymentProvider: PaymentProvider,
-) => string;
+export declare const modifyAppBuildGradle: (buildGradle: string, paymentProvider: PaymentProvider) => string;
 export declare const modifyProjectBuildGradle: (buildGradle: string) => string;
 interface Props {
-  paymentProvider?: PaymentProvider;
+    paymentProvider?: PaymentProvider;
 }
 declare const _default: ConfigPlugin<Props | undefined>;
 export default _default;
